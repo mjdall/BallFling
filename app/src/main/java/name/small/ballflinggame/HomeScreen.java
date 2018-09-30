7da4cd398809fffd10e5e2a050f781b06bf6d900
Morgan <mdd11@waikato.ac.nz>
2018-09-30 19:26:59 +1300

    another good commit name

--- app/src/main/java/name/small/ballflinggame/HomeScreen.java
+++ app/src/main/java/name/small/ballflinggame/HomeScreen.java
@@ -46,4 +46,11 @@ public class HomeScreen extends AppCompatActivity {
 
         }
     }
+
+    public void exitApp (View v) {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/name/small/ballflinggame/SoccerMumNameGeneratorGoodnessXD.java
+++ app/src/main/java/name/small/ballflinggame/SoccerMumNameGeneratorGoodnessXD.java
@@ -11,23 +11,19 @@ public class SoccerMumNameGeneratorGoodnessXD {
     public SoccerMumNameGeneratorGoodnessXD () {
         firstnames = new String[]{ "Carol", "Linda", "Wendy", "Amelia", "Mary", "Jenny", "Jessica",
                 "Lisa", "Susan", "Hannah", "Holly", "Shelly", "Barbara", "Gloria", "Lynn", "Leah", "Tracy", "Trish",
-                "Chelsie", "Danny(iel)", "Morgan", "Jenna", "Audrey", "Jodi", "Laura", "Christina", "Donna"};
+                "Chelsie", "Danny(iel)", "Morgan", "Jenna", "Audrey", "Jodi", "Laura", "Christina", "Donna", "BillEENA", "Tomette" };
         lastnames  = new String[]{ "Jones", "Jefferies", "Alberts", "Parker", "Simmons", "Stokes", "Dally",
                 "Blair", "Allen", "Carter", "Robinson", "Sanders", "Joyce", "Wakeman", "Smith", "Simmons", "Gonzalez",
-                "Turner", "Lopez", "Hernandez", "Craig", "Rogers", "Peterson", "Iverson", "Charles", "Wilkins", "Gillies"};
+                "Turner", "Lopez", "Hernandez", "Craig", "Rogers", "Peterson", "Iverson", "Charles", "Wilkins", "Gillies", "Levi" };
     }
 
     // gens a new name
     public SoccerMumName getName (ArrayList<SoccerMumName> currentHighscores) {
         SoccerMumName new_name = genName();
         for (int i = 0; i < currentHighscores.size(); i++) {
-            try {
-                if (new_name.compare(currentHighscores.get(i))) {
-                    new_name = genName();
-                    i = 0;
-                }
-            } catch (Exception e) {
+            if (new_name.compare(currentHighscores.get(i))) {
                 new_name = genName();
+                i = -1;
             }
         }
         return new_name;
@@ -35,9 +31,9 @@ public class SoccerMumNameGeneratorGoodnessXD {
 
     private SoccerMumName genName () {
         rand = new Random();
-        int index1 = rand.nextInt(27);
+        int index1 = rand.nextInt(29);
         rand = new Random();
-        int index2 = rand.nextInt(27);
+        int index2 = rand.nextInt(28);
         return new SoccerMumName(firstnames[index1], lastnames[index2], String.format("%03d%03d", index1, index2));
     }
 }
